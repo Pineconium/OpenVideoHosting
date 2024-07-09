399e752322515a0bedca141ec05de53a240f4652
Pineconium <101128219+Pineconium@users.noreply.github.com>
2024-07-09 14:36:47 +0100

    Updated Source Code -> July 9th 2024

--- src/db.php
+++ src/db.php
@@ -4,7 +4,8 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-$con = mysqli_connect("localhost", "root", "Qw3rTYUK0nT09", "register");
+/* Replace PASSWORDHERE and DATABASEHERE with proper information*/
+$con = mysqli_connect("localhost", "root", "PASSWORDHERE", "DATABASEHERE");
 if (mysqli_connect_errno())
   {
   	die("Fatal! Can not find a way to go to MySQL. Heres the error code for you: " . mysqli_connect_error());
